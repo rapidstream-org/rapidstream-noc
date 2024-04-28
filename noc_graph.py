2b6cc8b2c7cde2b02c7818f239fa07eb001f7041
Jake Ke <jackieke724@hotmail.com>
2024-04-27 20:01:13 -0700

    feat: add NocGraph class and vp1802's NoC graph

---
+++ noc_graph.py
@@ -0,0 +1,74 @@
+"""The NoC graph class."""
+
+__copyright__ = """
+Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+All rights reserved. The contributor(s) of this file has/have agreed to the
+RapidStream Contributor License Agreement.
+"""
+
+from pydantic import BaseModel
+
+
+class Node(BaseModel):
+    """Represents a node in the NoC graph."""
+
+    name: str
+
+
+class Edge(BaseModel):
+    """Represents an edge in the NoC graph."""
+
+    src: Node
+    dest: Node
+    bandwidth: int
+
+
+class NocGraph(BaseModel):
+    """Represents a NoC graph.
+
+    nmu_nodes: 2d array of all NMU nodes. Indexing follows Vivado.
+    nsu_nodes: 2d array of all NSU nodes. Indexing follows Vivado.
+    nps_vnoc_nodes: 2d array of all NPS nodes connected to NMUs and NSUs.
+                    Indexing follows Vivado.
+    nps_hnoc_nodes: 2d array of all interconnect NPS nodes bridging SLRs.
+    nps_slr0_nodes: one row of the NPS nodes connecting the bottom SLR0 to DDR and CIPS.
+    ncrb_nodes: 2d array of all NCRB nodes for east-west communication.
+    """
+
+    nmu_nodes: list[list[Node]]
+    nsu_nodes: list[list[Node]]
+    nps_vnoc_nodes: list[list[Node]]
+    nps_hnoc_nodes: list[list[Node]]
+    nps_slr0_nodes: list[Node]
+    ncrb_nodes: list[list[Node]]
+    edges: list[Edge]
+
+    def add_edge(self, edge: Edge) -> None:
+        """Add an edge."""
+        self.edges.append(edge)
+
+    def add_edges(self, edges: list[Edge]) -> None:
+        """Add a list of edges."""
+        for e in edges:
+            self.add_edge(e)
+
+    def get_all_nodes(self) -> list[str]:
+        """Get a list of all nodes' names.
+
+        Returns a list of strings.
+        """
+        all_nodes: list[str] = []
+        all_nodes += [n.name for row in self.nmu_nodes for n in row]
+        all_nodes += [n.name for row in self.nsu_nodes for n in row]
+        all_nodes += [n.name for row in self.nps_vnoc_nodes for n in row]
+        all_nodes += [n.name for row in self.nps_hnoc_nodes for n in row]
+        all_nodes += [n.name for n in self.nps_slr0_nodes]
+        all_nodes += [n.name for row in self.ncrb_nodes for n in row]
+        return all_nodes
+
+    def get_all_edges(self) -> list[tuple[str, str]]:
+        """Get a list of all edges without attributes.
+
+        Returns a list of tuples[str, str].
+        """
+        return [(edge.src.name, edge.dest.name) for edge in self.edges]
---
+++ vp1802_nocgraph.py
@@ -0,0 +1,347 @@
+"""Generates NoC graphs for FPGA devices."""
+
+__copyright__ = """
+Copyright (c) 2024 RapidStream Design Automation, Inc. and contributors.
+All rights reserved. The contributor(s) of this file has/have agreed to the
+RapidStream Contributor License Agreement.
+"""
+
+
+from typing import Any
+
+from noc_graph import Edge, NocGraph, Node
+
+
+def vp1802_nodes(
+    num_slr: int, num_col: int, rows_per_slr: list[int]
+) -> dict[str, list[Any]]:
+    """Creates all vp1802 NoC's nodes.
+
+    Returns a dictionary of all node types in the NocGraph.
+    """
+    num_row = sum(rows_per_slr)
+    # each slr has two top and two bottom rows
+    # except for slr0 which only has two top rows
+    num_inter_rows = num_slr * 2 * 2 - 2
+
+    all_nodes: dict[str, list[Any]] = {
+        "nmu_nodes": [],
+        "nsu_nodes": [],
+        "nps_vnoc_nodes": [],
+        "nps_hnoc_nodes": [],
+        "nps_slr0_nodes": [],
+        "ncrb_nodes": [],
+    }
+
+    # create nmu and nsu nodes
+    for x in range(num_col):
+        nmu_nodes_col = []
+        nsu_nodes_col = []
+        for y in range(num_row):
+            nmu_nodes_col.append(Node(name=f"nmu_x{x}y{y}"))
+
+            nsu_nodes_col.append(Node(name=f"nsu_x{x}y{y}"))
+        all_nodes["nmu_nodes"].append(nmu_nodes_col)
+        all_nodes["nsu_nodes"].append(nsu_nodes_col)
+
+    # create nps_vnoc nodes
+    for x in range(num_col):
+        col = []
+        for y in range(num_row * 2):
+            col.append(Node(name=f"nps_vnoc_x{x}y{y}"))
+        all_nodes["nps_vnoc_nodes"].append(col)
+
+    # create interconnect nps nodes
+    for x in range(num_col):
+        col = []
+        for y in range(num_inter_rows):
+            col.append(Node(name=f"nps_x{x}y{y}"))
+        all_nodes["nps_hnoc_nodes"].append(col)
+
+    # create ncrb nodes for connecting interconnect rows within each slr
+    # NoC Clock Re-convergent Buffer (NCRB)
+    for x in range(num_col - 1):
+        col = []
+        for y in range(num_inter_rows):
+            col.append(Node(name=f"ncrb_x{x}y{y}"))
+        all_nodes["ncrb_nodes"].append(col)
+
+    # create bottom nps nodes connecting SLR0 to DDR and CIPS
+    for x in range(num_col * 2):
+        all_nodes["nps_slr0_nodes"].append(Node(name=f"nps_slr0_x{x}y0"))
+
+    return all_nodes
+
+
+def create_bidir_edges(n1: Node, n2: Node, **kwargs: int) -> list[Edge]:
+    """Creates a bidirectional edge between nodes n1 and n2 with attributes.
+
+    Returns a list of two edges.
+    """
+    return [
+        Edge(src=n1, dest=n2, **kwargs),
+        Edge(src=n2, dest=n1, **kwargs),
+    ]
+
+
+def create_nmu_nsu_edges(
+    G: NocGraph, num_col: int, rows_per_slr: list[int]
+) -> list[Edge]:
+    """Creates edges between nmu, nsu, and nps_vnoc within the subgroup.
+
+    Returns a list of edges.
+    """
+    num_row = sum(rows_per_slr)
+
+    edges = []
+
+    # create nmu <-> nps_vnoc <-> nps_vnoc <-> nsu edges
+    y_at_slr_boundary = [
+        sum(rows_per_slr[: i + 1]) - 1 for i in range(len(rows_per_slr))
+    ]
+    y = 0
+    for x in range(num_col):
+        for y in range(num_row):
+            # nmu <-> nps_vnoc
+            edges += create_bidir_edges(
+                G.nmu_nodes[x][y], G.nps_vnoc_nodes[x][y * 2 + 1], bandwidth=16000
+            )
+
+            # nsu <-> nps_vnoc
+            edges += create_bidir_edges(
+                G.nsu_nodes[x][y], G.nps_vnoc_nodes[x][y * 2], bandwidth=16000
+            )
+
+            # horizontal nps_vnoc <-> nps_vnoc
+            edges += create_bidir_edges(
+                G.nps_vnoc_nodes[x][y * 2],
+                G.nps_vnoc_nodes[x][y * 2 + 1],
+                bandwidth=16000,
+            )
+
+            # vertical nps_vnoc <-> nps_vnoc
+            if y not in y_at_slr_boundary:
+                edges += create_bidir_edges(
+                    G.nps_vnoc_nodes[x][y * 2],
+                    G.nps_vnoc_nodes[x][(y + 1) * 2],
+                    bandwidth=16000,
+                )
+
+                edges += create_bidir_edges(
+                    G.nps_vnoc_nodes[x][y * 2 + 1],
+                    G.nps_vnoc_nodes[x][(y + 1) * 2 + 1],
+                    bandwidth=16000,
+                )
+    return edges
+
+
+def create_nps_hnoc_edges(G: NocGraph, num_slr: int, num_col: int) -> list[Edge]:
+    """Creates edges for horizontal and vertical nps nodes.
+
+    Returns a list of edges.
+    """
+    edges = []
+
+    y = 7
+    for slr in range(num_slr):
+        for r in range(2):
+            for x in range(num_col):
+                # connect top interconnect nps nodes to vnoc nps nodes
+                edges += create_bidir_edges(
+                    G.nps_hnoc_nodes[x][slr * 4 + r],
+                    G.nps_vnoc_nodes[x][y * 2 - 2 + r],
+                    bandwidth=16000,
+                )
+                # print(f"connecting nps_x{x}y{slr*4+r} <-> nps_vnoc_x{x}y{y*2-2+r}")
+
+                # connect lower interconnect nps nodes to vnoc nps nodes
+                if slr < num_slr - 1:
+                    edges += create_bidir_edges(
+                        G.nps_hnoc_nodes[x][slr * 4 + 2 + r],
+                        G.nps_vnoc_nodes[x][y * 2 + r],
+                        bandwidth=16000,
+                    )
+                    # print(f"connecting nps_x{x}y{slr*4+2+r} <-> nps_vnoc_x{x}y{y*2+r}")
+        y += 6
+
+    # cross-slr edges
+    # abstracted away the NoC Inter-Die Bridge (NIDB) nodes
+    for slr in range(num_slr - 1):
+        for r in range(2):
+            for x in range(num_col):
+                edges += create_bidir_edges(
+                    G.nps_hnoc_nodes[x][slr * 4 + r],
+                    G.nps_hnoc_nodes[x][slr * 4 + r + 2],
+                    bandwidth=16000,
+                )
+                # print(f"nps_x{x}y{slr*4+r} <-> nps_x{x}y{slr*4+r+2}")
+
+    return edges
+
+
+def create_ncrb_edges(G: NocGraph, num_slr: int, num_col: int) -> list[Edge]:
+    """Creates edges for ncrb nodes.
+
+    Returns a list of edges.
+    """
+    edges = []
+
+    for y in range(num_slr * 2 - 1):
+        for x in range(num_col - 1):
+            # west direction
+            edges.append(
+                Edge(
+                    src=G.ncrb_nodes[x][y * 2],
+                    dest=G.nps_hnoc_nodes[x][y * 2],
+                    bandwidth=11500,
+                )
+            )
+            edges.append(
+                Edge(
+                    src=G.ncrb_nodes[x][y * 2],
+                    dest=G.nps_hnoc_nodes[x][y * 2 + 1],
+                    bandwidth=11500,
+                )
+            )
+            edges.append(
+                Edge(
+                    src=G.nps_hnoc_nodes[x + 1][y * 2],
+                    dest=G.ncrb_nodes[x][y * 2],
+                    bandwidth=11500,
+                )
+            )
+            edges.append(
+                Edge(
+                    src=G.nps_hnoc_nodes[x + 1][y * 2 + 1],
+                    dest=G.ncrb_nodes[x][y * 2],
+                    bandwidth=11500,
+                )
+            )
+            # print(f"ncrb_x{x}y{y*2} -> nps_x{x}y{y*2}")
+            # print(f"ncrb_x{x}y{y*2} -> nps_x{x}y{y*2+1}")
+            # print(f"nps_x{x+1}y{y*2} -> ncrb_x{x}y{y*2}")
+            # print(f"nps_x{x+1}y{y*2+1} -> ncrb_x{x}y{y*2}")
+
+            # east direction
+            edges.append(
+                Edge(
+                    src=G.nps_hnoc_nodes[x][y * 2],
+                    dest=G.ncrb_nodes[x][y * 2 + 1],
+                    bandwidth=11500,
+                )
+            )
+            edges.append(
+                Edge(
+                    src=G.nps_hnoc_nodes[x][y * 2 + 1],
+                    dest=G.ncrb_nodes[x][y * 2 + 1],
+                    bandwidth=11500,
+                )
+            )
+            edges.append(
+                Edge(
+                    src=G.ncrb_nodes[x][y * 2 + 1],
+                    dest=G.nps_hnoc_nodes[x + 1][y * 2],
+                    bandwidth=11500,
+                )
+            )
+            edges.append(
+                Edge(
+                    src=G.ncrb_nodes[x][y * 2 + 1],
+                    dest=G.nps_hnoc_nodes[x + 1][y * 2 + 1],
+                    bandwidth=11500,
+                )
+            )
+            # print(f"nps_x{x}y{y*2} -> ncrb_x{x}y{y*2+1}")
+            # print(f"nps_x{x}y{y*2+1} -> ncrb_x{x}y{y*2+1}")
+            # print(f"ncrb_x{x}y{y*2+1} -> nps_x{x+1}y{y*2}")
+            # print(f"ncrb_x{x}y{y*2+1} -> nps_x{x+1}y{y*2+1}")
+
+    return edges
+
+
+def create_nps_slr0_edges(G: NocGraph, num_col: int) -> list[Edge]:
+    """Creates edges for nps_slr0_nodes.
+
+    Returns a list of edges.
+    """
+    edges = []
+
+    # slr0's nps_vnoc <-> nps_slr0
+    for x in range(num_col * 2):
+        edges += create_bidir_edges(
+            G.nps_slr0_nodes[x], G.nps_vnoc_nodes[int(x / 2)][x % 2], bandwidth=16000
+        )
+        # print(f"nps_slr0_x{x}y0", f"nps_vnoc_x{int(x/2)}y{x%2}")
+
+    # ddr and CIPS crossbar
+    for x in range(num_col):
+        edges += create_bidir_edges(
+            G.nps_slr0_nodes[x * 2], G.nps_slr0_nodes[x * 2 + 1], bandwidth=16000
+        )
+        # print(f"nps_slr0_x{x*2+1}y0 <-> nps_slr0_x{x*2}y0")
+
+    for x in range(num_col - 1):
+        for r in range(2):
+            edges += create_bidir_edges(
+                G.nps_slr0_nodes[x * 2 + r],
+                G.nps_slr0_nodes[x * 2 + r + 2],
+                bandwidth=16000,
+            )
+            # print(f"nps_slr0_x{x*2+r}y0 <-> nps_slr0_x{x*2+r+2}y0")
+
+    return edges
+
+
+def vp1802_edges(
+    G: NocGraph, num_slr: int, num_col: int, rows_per_slr: list[int]
+) -> list[Edge]:
+    """Creates all vp1802 NoC's edges.
+
+    Returns a list of all edges.
+    """
+    sum(rows_per_slr)
+
+    edges = []
+    edges += create_nmu_nsu_edges(G, num_col, rows_per_slr)
+    edges += create_nps_hnoc_edges(G, num_slr, num_col)
+    edges += create_ncrb_edges(G, num_slr, num_col)
+    edges += create_nps_slr0_edges(G, num_col)
+    return edges
+
+
+def vp1802_nocgraph(num_slr: int, num_col: int, rows_per_slr: list[int]) -> NocGraph:
+    """Generates VP1802's NoC graph.
+
+    Example:
+    >>> import networkx as nx
+    >>> num_slr = 4
+    >>> num_col = 4
+    >>> rows_per_slr = [7, 6, 6, 6]
+    >>> G = vp1802_nocgraph(num_slr, num_col, rows_per_slr)
+    >>> nx_g = nx.DiGraph()
+    >>> nx_g.add_nodes_from(G.get_all_nodes())
+    >>> nx_g.add_edges_from(G.get_all_edges())
+    >>> nx.shortest_path(nx_g, source="nmu_x0y0", target="nsu_x0y0")
+    ['nmu_x0y0', 'nps_vnoc_x0y1', 'nps_vnoc_x0y0', 'nsu_x0y0']
+    >>> nx.shortest_path(nx_g, source="nmu_x0y0", target="nsu_x1y0")
+    ['nmu_x0y0', 'nps_vnoc_x0y1', 'nps_slr0_x1y0', 'nps_slr0_x3y0', 'nps_vnoc_x1y1', \
+        'nps_vnoc_x1y0', 'nsu_x1y0']
+    """
+
+    nodes = vp1802_nodes(num_slr, num_col, rows_per_slr)
+
+    # Create a directed graph
+    G = NocGraph(
+        nmu_nodes=nodes["nmu_nodes"],
+        nsu_nodes=nodes["nsu_nodes"],
+        nps_vnoc_nodes=nodes["nps_vnoc_nodes"],
+        nps_hnoc_nodes=nodes["nps_hnoc_nodes"],
+        nps_slr0_nodes=nodes["nps_slr0_nodes"],
+        ncrb_nodes=nodes["ncrb_nodes"],
+        edges=[],
+    )
+
+    edges = vp1802_edges(G, num_slr, num_col, rows_per_slr)
+    G.add_edges(edges)
+
+    return G
